f6159a7798423b461c2a7484a4fbaa5843304cb0
zqchen90 <chenzhaoqun@gmail.com>
2015-01-05 15:03:26 +0800

    Path sum
    A root-to-LEAF path

---
+++ path-sum/path-sum.cpp
@@ -0,0 +1,23 @@
+﻿/**
+ * Given the below binary tree and sum and return a ROOT-TO-LEAF path which sum is the given one
+ *
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if (NULL == root) {
+            return false;
+        }
+        if (root->val == sum && NULL == root->left && NULL == root->right) {
+            return true;
+        }
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
