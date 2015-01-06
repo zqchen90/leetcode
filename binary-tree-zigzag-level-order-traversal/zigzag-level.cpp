422faab701f839f6cfddf07ec8cf264dda50635e
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-06 17:32:13 +0800

    Zigzag level traversal add reverse

--- binary-tree-zigzag-level-order-traversal/zigzag-level.cpp
+++ binary-tree-zigzag-level-order-traversal/zigzag-level.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
@@ -47,11 +49,40 @@ vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
             cur_level = 0;
         }        
     }
+    // Become zigzag
+    bool reverse_flag = false;
+    for (int i = 0; i < ret.size(); ++i) {
+        if (reverse_flag) {
+            reverse(ret[i].begin(), ret[i].end());
+        }
+        reverse_flag = !reverse_flag;
+    }
     return ret;
 }
 
+void print_2d_vector(const vector<vector<int> > &mat) {
+    for (int i = 0; i < mat.size(); ++i) {
+        for (int j = 0; j < mat[i].size(); ++j) {
+            cout<<mat[i][j]<<",";
+        }
+        cout<<endl;
+    }
+}
+
+
 int main() {
-    
+    TreeNode root(3);
+    TreeNode level11(9);
+    TreeNode level12(20);
+    TreeNode level21(15);
+    TreeNode level22(7);
+    root.left = &level11;
+    root.right = &level12;
+    level12.left = &level21;
+    level12.right = &level22;
+
+    vector<vector<int> > ret = zigzagLevelOrder(&root);
+    print_2d_vector(ret);
 
     return 0;
 }
