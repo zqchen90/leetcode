9031c5f7524ce65583aa90ef7423281b2cfb40e6
zqchen90 <chenzhaoqun@gmail.com>
2015-01-06 19:33:00 +0800

    1st commit min depth of binary tree

---
+++ minimum-depth-of-binary-tree/minmum-depth-of-binary-tree.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        if (NULL == root) {
+            return 0;
+        }
+        // a leaf
+        if (NULL == root->left && NULL == root->right) {
+            return 1;
+        }
+        if (NULL == root->left) {
+            return 1 + minDepth(root->right);
+        }
+        if (NULL == root->right) {
+            return 1 + minDepth(root->left);
+        }
+        int min_depth_left = minDepth(root->left);
+        int min_depth_right = minDepth(root->right);
+        int min_depth = min_depth_left < min_depth_right ? min_depth_left : min_depth_right;
+        return min_depth + 1;
+    }
+};
