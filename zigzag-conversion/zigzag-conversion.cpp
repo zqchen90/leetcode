d7ac12a213b08dd546ffee121904830e76f9f176
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-07 13:27:54 +0800

    1st Zigzag Conversion

---
+++ zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string convert(string s, int nRows) {
+    string ret = "";
+    int first = 0;
+    int second = 0;
+    int interval = 2 * nRows - 2;
+    // Row 0
+    int pos = 0;
+    while (pos < s.size()) {
+        ret.append(1, s[pos]);
+        pos += interval;
+    }
+    // Row 1 - (nRows - 2)
+    for (int row = 1; row < nRows - 1; ++row) {
+        int first = row;
+        int second = interval - row;
+        while (first < s.size() || second < s.size()) {
+            if (first < s.size()) {
+                ret.append(1, s[first]);
+            }
+            if (second < s.size()) {
+                ret.append(1, s[second]);
+            }
+            first += interval;
+            second += interval;
+        }
+    }
+    // Row nRows - 1
+    pos = nRows - 1;
+    while (pos < s.size()) {
+        ret.append(1, s[pos]);
+        pos += interval;
+    }
+    return ret;
+}
+
+void testConvert(string input, int nRows, string result) {
+    string output = convert(input, nRows);
+    if (output == result) {
+        cout<<"Pass"<<endl;
+    } else {
+        cout<<"Fail"<<" "<<output<<" "<<result<<endl;
+    }
+}
+
+int main() {
+    testConvert("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR");    
+    testConvert("PAYPALISHIRING", 4, "PINALSIGYAHRPI");    
+    return 0;
+}
+
---
+++ zigzag-conversion/zigzag-conversion.sh
@@ -0,0 +1,2 @@
+g++ zigzag-conversion.cpp -o zigzag-conversion
+./zigzag-conversion
