2e72e9bf477d5bb64be6bb02672f95b7eb7218e0
zqchen90 <chenzhaoqun@gmail.com>
2015-01-08 14:39:15 +0800

    AC 2015-1-8
    Solved by standard DP

---
+++ unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (1 == m || 1 == n) {
+            return 1;
+        }
+        if (2 == m && 2 == n) {
+            return 2;
+        }
+        vector<vector<int> > dp (m + 1, vector<int> (n + 1, 0));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (1 == i || 1 == j) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                dp[i][j] = dp[i-1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m][n];
+    }
+};
