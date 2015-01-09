f066fa124365e7ce802be22fdd858871e2fdd485
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-09 19:34:18 +0800

    AC 2015-1-9

--- recover-binary-search-tree/recover-binary-search-tree.cpp
+++ recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -9,15 +9,6 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-void detect(TreeNode *prev, TreeNode *cur, TreeNode **swap1, TreeNode **swap2) {
-    if (NULL != prev && prev->val > cur->val) {
-        if (*swap1 == NULL) {
-            *swap1 = prev;
-        }
-        *swap2 = cur;
-    }
-}
-
 void print_TreeNode(TreeNode *cur, TreeNode *prev) {
     if (NULL == prev) {
         cout<<"[NULL";
@@ -32,6 +23,15 @@ void print_TreeNode(TreeNode *cur, TreeNode *prev) {
     cout<<endl;
 }
 
+void detect(TreeNode *prev, TreeNode *cur, TreeNode **swap1, TreeNode **swap2) {
+    if (prev != NULL && prev->val > cur->val) {
+        if (*swap1 == NULL) {
+            *swap1 = prev;
+        }
+        *swap2 = cur;
+    }
+}
+
 void recoverTree(TreeNode *root) {
     TreeNode *cur = root;
     TreeNode *prev = NULL;
@@ -41,21 +41,21 @@ void recoverTree(TreeNode *root) {
     while (cur != NULL) {
         print_TreeNode(cur, prev);
         if (cur->left == NULL) {
-            detect(cur, prev, &swap1, &swap2);
+            detect(prev, cur, &swap1, &swap2);
             prev = cur;
             cur = cur->right;
         } else {
-            prev = cur->left;
-            while (prev->right != NULL && prev->right != cur) {
-                prev = prev->right;           
+            TreeNode *node = cur->left;
+            while (node->right != NULL && node->right != cur) {
+                node = node->right;           
             }
             //print_TreeNode(cur, prev);
-            if (prev->right == NULL) {
-                prev->right = cur;
+            if (node->right == NULL) {
+                node->right = cur;
                 cur = cur->left;
             } else {
                 detect(prev, cur, &swap1, &swap2);
-                prev->right = NULL;
+                node->right = NULL;
                 prev = cur;
                 cur = cur->right;
             }
@@ -63,7 +63,7 @@ void recoverTree(TreeNode *root) {
     }
     int tmp = swap1->val;
     swap1->val = swap2->val;
-    
+    swap2->val = tmp; 
 }
 
 
@@ -73,7 +73,9 @@ int main() {
     root.left = &left;
     recoverTree(&root);
 
-    TreeNode root2(0);
+    cout<<endl;
+
+    TreeNode root2(2);
     TreeNode right(1);
     root2.right = &right;
     recoverTree(&root2);
