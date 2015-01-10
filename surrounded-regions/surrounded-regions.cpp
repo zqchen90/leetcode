e9afc510a24cb6aa9a03b9e7be763789903a2a63
zqchen90 <chenzhaoqun@gmail.com>
2015-01-10 20:04:15 +0800

    AC 2015-1-10

---
+++ surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,91 @@
+﻿class Solution {
+public:
+  int row;
+    int col;
+    vector<pair<int, int> > move_step; 
+
+    void bfs(vector<vector<char> > &board, int r, int c) {
+        queue<pair<int, int> > qe;
+        qe.push({r, c});
+
+        while(!qe.empty()) {        
+            r = qe.front().first;
+            c = qe.front().second;
+            qe.pop();
+            for(int i = 0; i < 4; i++) {
+                int rr = r + move_step[i].first;
+                int cc = c + move_step[i].second; 
+                if(rr >= 0 && rr < row
+                        && cc >= 0 && cc < col
+                         && board[rr][cc] == 'O') {
+                    board[rr][cc] = '+';
+                    qe.push({rr, cc});
+                }
+            }
+        }
+
+        return;
+    }
+
+    void solve(vector<vector<char> > &board) {
+        if (board.empty())
+            return;
+
+        row = board.size();        
+        col = board[0].size();        
+
+        move_step.resize(4);
+        move_step[0] = {0,-1};
+        move_step[1] = {0,1};
+        move_step[2] = {-1,0};
+        move_step[3] = {1,0};
+
+        // BFS from four edges and mark  non-surrounded(dead) cells with '+' sign
+
+        // top edge
+        for(int i = 0; i < col; i++) {
+            if(board[0][i] == 'O') {
+                board[0][i] = '+';
+                bfs(board, 0, i);
+            }
+        }
+
+        // bottom edge
+        for(int i = 0; i < col; i++) {
+            if(board[row-1][i] == 'O') {
+                board[row-1][i] = '+';
+                bfs(board, row-1, i);
+            }
+        }
+
+        // left edge
+        for(int i = 1; i < row-1; i++) {
+            if(board[i][0] == 'O') {
+                board[i][0] = '+';
+                bfs(board, i, 0);
+            }
+        }
+
+        // right edge
+        for(int i = 1; i < row-1; i++) {
+            if(board[i][col-1] == 'O') {
+                board[i][col-1] = '+';
+                bfs(board, i, col-1);
+            }
+        }
+
+        // then scan all the cells, recover live cells and flip dead cells 
+        for(int i = 0; i < row; i++) 
+            for(int j = 0; j < col; j++) {
+                char cur = board[i][j];
+                // recover live cell marked with '+' to 'O'
+                if(cur == '+') 
+                    board[i][j] = 'O';
+                // flip dead cell to '*'
+                else if(cur == 'O') 
+                    board[i][j] = 'X';
+            }
+
+        return;
+    }
+};
