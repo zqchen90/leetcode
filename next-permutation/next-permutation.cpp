92e53e3aecbac1efeca2ae3aa1a6c30fab4e4adf
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-10 18:07:44 +0800

    Compile and toy utest, thinks added

---
+++ next-permutation/next-permutation.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "print_vector.h"
+#include "check_vector.h"
+
+using namespace std;
+
+void nextPermutation(vector<int> &num) {
+    if (1 >= num.size()) {
+        return;
+    }
+    int partition_number = num.size() - 2;
+    while (partition_number > 0) {
+        if (num[partition_number] < num[partition_number + 1]) {
+            break;
+        }
+        partition_number--;
+    }
+    if (partition_number < 0) {
+        return reverse(num.begin(), num.end());
+    }
+    int change_number = num.size() - 1;
+    while (change_number > partition_number) {
+        if (num[change_number] > num[partition_number]) {
+            break;
+        }
+    }
+    swap(num[change_number], num[partition_number]);
+    reverse(num.begin() + partition_number + 1, num.end());
+    return;
+}
+
+void testNextPermutation(vector<int> input, vector<int> output) {
+    nextPermutation(input);
+    if (check_vector(input, output)) {
+        cout<<"Pass"<<endl;
+    } else {
+        cout<<"Fail, output: ";
+        print_vector(input);
+        cout<<" result: ";
+        print_vector(output);
+        cout<<endl;
+    }
+}
+
+int main() {
+    int input1[] = {1,2,3};
+    int output1[] = {1,3,2};
+    vector<int> inputv1 (input1, input1+3);
+    vector<int> outputv1 (output1, output1+3);
+    testNextPermutation(inputv1, outputv1);
+
+    int input2[] = {1,1,3};
+    int output2[] = {1,3,1};
+    vector<int> inputv2 (input2, input2+3);
+    vector<int> outputv2 (output2, output2+3);
+    testNextPermutation(inputv2, outputv2);
+    return 0;
+}
+
---
+++ next-permutation/thinks.md
@@ -0,0 +1,5 @@
+# Solutions
+ - From right to left, find the first digit (partition_number) which voilate the increase trend
+ - From right to left, find the first digit which is larger than partition_number, called change_number
+ - Swap partition_number and change_number
+ - Reverse all the digits on the right side of partition_number
