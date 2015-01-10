092708f520425043c849ea492a17463b6cefd53e
zqchen90 <chenzhaoqun@gmail.com>
2015-01-10 21:13:47 +0800

    Add string split
    http://stackoverflow.com/questions/236129/split-a-string-in-c

--- czq_utilities/czqstring.h
+++ czq_utilities/czqstring.h
@@ -1,4 +1,25 @@
 #ifndef CZQSTRING_H
 #define CZQSTRING_H
 
+#include <vector>
+#include <string>
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+		if (!item.empty()) {
+			elems.push_back(item);
+		}
+    }
+    return elems;
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
 #endif
