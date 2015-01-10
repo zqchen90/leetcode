e8121cdf3fcc15ec3967d0b49882202026797037
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-10 20:50:00 +0800

    Compile

---
+++ binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+#include "TreeNode.h"
+
+using namespace std;
+
+class BSTIterator {
+    public:
+        BSTIterator (TreeNode *root) {
+            if (NULL != root) {
+                TreeNode *p = root;
+                while (NULL != root) {
+                    s.push(p);
+                    p = p->left;
+                }
+            }
+        }
+
+        bool hasNext() {
+            return !s.empty();
+        }
+
+        int next() {
+            TreeNode *p = s.top();
+            s.pop();
+            TreeNode *pr = p->right;
+            while (NULL != pr) {
+                s.push(pr);
+                pr = pr->left;
+            }
+            return p->val;
+        }
+    private:
+        stack<TreeNode *> s;
+};
+
+
+int main() {
+    
+
+    return 0;
+}
+
