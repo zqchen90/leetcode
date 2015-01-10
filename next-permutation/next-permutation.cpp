39937be6a96490c0463847d411bd0242032ad814
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-10 18:12:30 +0800

    AC 2015-1-10

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -11,7 +11,7 @@ void nextPermutation(vector<int> &num) {
         return;
     }
     int partition_number = num.size() - 2;
-    while (partition_number > 0) {
+    while (partition_number >= 0) {
         if (num[partition_number] < num[partition_number + 1]) {
             break;
         }
@@ -25,6 +25,7 @@ void nextPermutation(vector<int> &num) {
         if (num[change_number] > num[partition_number]) {
             break;
         }
+        change_number--;
     }
     swap(num[change_number], num[partition_number]);
     reverse(num.begin() + partition_number + 1, num.end());
@@ -56,6 +57,13 @@ int main() {
     vector<int> inputv2 (input2, input2+3);
     vector<int> outputv2 (output2, output2+3);
     testNextPermutation(inputv2, outputv2);
+     
+    int input3[] = {1,1};
+    int output3[] = {1,1};
+    vector<int> inputv3 (input3, input3+2);
+    vector<int> outputv3 (output3, output3+2);
+    testNextPermutation(inputv3, outputv3);
     return 0;
+
 }
 
