016fc540423e61c7010a83781e91741ce04a801d
zqchen90 <chenzhaoqun@gmail.com>
2015-01-10 14:40:34 +0800

    AC 2015-1-10, one pass and two passes solution

---
+++ sort-colors/sort-colors-one-pass.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    void sortColors(int A[], int n) {
+		int red_cnt = 0;
+		int blue_cnt = n - 1;
+        for (int i = 0; i <= blue_cnt;) {
+            if (A[i] == 0) {
+				swap(A[red_cnt], A[i]);
+				red_cnt++;
+				i++; // i must larger than red_cnt
+			} else if (A[i] == 2) {
+				swap(A[blue_cnt], A[i]);
+				blue_cnt--;
+			} else {
+				i++;
+			}
+        }
+    }
+};
---
+++ sort-colors/sort-colors-two-passes.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    void sortColors(int A[], int n) {
+        int color_cnt[] = {0,0,0};
+        for (int i = 0; i < n; ++i) {
+            color_cnt[A[i]]++;
+        }
+        int i = 0;
+        for (int color = 0; color <=2; ++color) {
+            for (int j = 0; j < color_cnt[color]; ++j) {
+                A[i + j] = color;
+            }
+            i += color_cnt[color];
+        }
+    }
+};
