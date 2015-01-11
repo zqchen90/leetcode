7f67c679f86cc3b8972a4e01f052b2e91457dc6f
zqchen90 <chenzhaoqun@gmail.com>
2015-01-11 10:26:17 +0800

    AC 2015-1-11

---
+++ covert-sorted-array-to-binary-search-tree/covert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+﻿/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num, int start, int end) {
+        if (start > end) {
+            return NULL;
+        }
+        if (start == end) {
+            TreeNode *p = new TreeNode(num[start]);
+            return p;
+        }
+        int mid = (start + end) / 2;
+        TreeNode *root = new TreeNode(num[mid]);
+        TreeNode *left = sortedArrayToBST(num, start, mid - 1);
+        TreeNode *right = sortedArrayToBST(num, mid + 1, end);
+        root->left = left;
+        root->right = right;
+        return root;
+    }
+    
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        return sortedArrayToBST(num, 0, num.size() - 1);
+    }
+};
