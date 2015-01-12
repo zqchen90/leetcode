1994301e89ac7aded6cd2c34716bc30271056103
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-12 11:42:16 +0800

    Compile and test 3

---
+++ subset/subset.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include "czqvector.h"
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int> > get_subsets(vector<int> &S, int index) {
+    vector<vector<int> > all_sets;
+    if (index == S.size()) {
+        vector<int> tmp;
+        all_sets.push_back(tmp);
+    } else {
+        vector<vector<int> > some_sets = get_subsets(S, index + 1);
+        for (int i = 0; i < some_sets.size(); ++i) {
+            vector<int> set_index;
+            set_index.push_back(S[index]);
+            for (int j = 0; j < some_sets[i].size(); ++j) {
+                set_index.push_back(some_sets[i][j]);
+            }
+            all_sets.push_back(some_sets[i]);
+            all_sets.push_back(set_index);
+        }
+    }
+    return all_sets;      
+}
+
+vector<vector<int> > subsets(vector<int> &S) {
+    sort(S.begin(), S.end());
+    return get_subsets(S, 0);
+}
+
+int main() {
+    vector<int> input = build_int_vector("1,2,3", ',');
+    print_vector(input);
+    cout<<endl<<endl;
+    vector<vector<int> > ret = subsets(input);
+    print_2d_vector(ret);
+
+    return 0;
+}
+
