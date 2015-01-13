15100c4c7ef6f899db481fef98e29de22aa86351
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-13 18:59:21 +0800

    AC 2015-1-13

---
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,38 @@
+﻿class Solution {
+public:
+    int longestConsecutive(vector<int> &num) {
+        unordered_map<int, bool> used;
+        for (int i = 0; i < num.size(); ++i) {
+            used[num[i]] = false;
+        }
+        int longest = 1;
+        for (int i = 0; i < num.size(); ++i) {
+            if (used[num[i]]) {
+                continue;
+            }
+            int length = 1;
+            used[num[i]] = true;
+            // Try right side
+            for (int j = num[i] + 1; ; j++) {
+                if (used.find(j) == used.end()) {
+                    break;
+                } else {
+                    length++;
+                    used[j] = true;
+                }
+            }
+            // Try left side
+            for (int j = num[i] - 1; ; j--) {
+                if (used.find(j) == used.end()) {
+                    break;
+                } else {
+                    length++;
+                    used[j] = true;
+                }
+            }
+            // Update longest
+            longest = max(longest, length);
+        }
+        return longest;
+    }
+};
---
+++ longest-consecutive-sequence/thinks.md
@@ -0,0 +1,14 @@
+# Question
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+For example,
+
+Given [100, 4, 200, 1, 3, 2],
+
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+
+Your algorithm should run in O(n) complexity.
+
+# Solution
+
+Traversal every element and check its right and left sides to form a consecutive sequence. Use unordered_map to make searching O(1).
