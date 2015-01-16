c710d8f7829f74fb8400075e9131f1d047be5401
Zhaoqun Chen <chenzhaoqun@gmail.com>
2015-01-16 13:25:29 +0800

    Modify the definition of template class

--- czq_algorithms/range_minimum_query/range-minimum-query.h
+++ czq_algorithms/range_minimum_query/range-minimum-query.h
@@ -3,57 +3,69 @@
 
 #include "segment_tree.h"
 
-template <typename T>
+template <class T>
 class RangeMinimumQuery {
 public:
-    template <typename T>
-    RangeMinimumQuery(std::vector<T> &Data) : st(SegmentTree(Data.size())),
-                                              data(vector(Data)) {
-    }
-
-    template <typename T>
-    void initialize() {
-        st.build_tree(data);
-    }
-
-    template <typename T> 
-    int get_range_minimum_index(int left, int right) {
-        if (left < 0 || right > data.size() || left > right) {
-            return -1;
-        }
-        if (left == right) {
-            return left;
-        }
-        return find_range_minimum_index(array, st, 0, size - 1, qstart, qend, 0);
-    }
+    RangeMinimumQuery(std::vector<T> &Data);
+    void initialize();
+    int get_range_minimum_index(int left, int right);
                                   
 private:
-    template <typename T>
-    int minValIndex(int i, int j) {
-        if (i == -1) return j;
-        if (j == -1) return i;
-        return (data[i] < data[j]) ? i : j;
-    }
-
-    template <typename T>
-    int find_range_minimum_index(int start, int end, int qstart, int qend, int index) {
-        // Now the segment node is a part of the query range
-        if (start >= qstart && end <= qend) {
-            return st.get_index(index);
-        }
-        // segment node is out of range
-        if (end < qstart || start > qend) {
-            return -1;
-        }
-        // search left child tree and right child tree
-        int middle = (start + end) / 2;
-        int left_index = find_range_minimum_index(start, middle, qstart, qend, 2 * index + 1);
-        int right_index = find_range_minimum_index(middle + 1, end, qstart, qend, 2 * index + 2);
-        return minValIndex(left_index, right_index);
-    }
+    int minValIndex(int i, int j);
+    int find_range_minimum_index(int start, int end, int qstart, int qend, int index);
 
     SegmentTree<T> st;
     std::vector<T> data;
 };
 
+template <class T> RangeMinimumQuery<T>::RangeMinimumQuery(
+    std::vector<T> &Data) : st(SegmentTree(Data.size())), data(vector(Data)) {
+}
+
+template <class T> RangeMinimumQuery<T>:: void initialize() {
+    st.build_tree(data);
+}
+
+template <class T> RangeMinimumQuery<T>::int get_range_minimum_index(int left, int right) {
+    if (left < 0 || right > data.size() || left > right) {
+        return -1;
+    }
+    if (left == right) {
+        return left;
+    }
+    return find_range_minimum_index(array, st, 0, size - 1, qstart, qend, 0);
+}
+
+template <class T> RangeMinimumQuery<T>::int minValIndex(int i, int j) {
+    if (i == -1) return j;
+    if (j == -1) return i;
+    return (data[i] < data[j]) ? i : j;
+}
+
+template <class T> RangeMinimumQuery<T>::int find_range_minimum_index(
+                                                int start,
+                                                int end, 
+                                                int qstart, 
+                                                int qend, 
+                                                int index) {
+    // Now the segment node is a part of the query range
+    if (start >= qstart && end <= qend) {
+        return st.get_index(index);
+    }
+    // segment node is out of range
+    if (end < qstart || start > qend) {
+        return -1;
+    }
+    // search left child tree and right child tree
+    int middle = (start + end) / 2;
+    int left_index = find_range_minimum_index(start, middle, 
+                                                qstart, qend,
+                                                2 * index + 1);
+    int right_index = find_range_minimum_index(middle + 1, end,
+                                                qstart, qend,
+                                                2 * index + 2);
+    return minValIndex(left_index, right_index);
+}
+
 #endif
+
--- czq_algorithms/range_minimum_query/segment_tree.h
+++ czq_algorithms/range_minimum_query/segment_tree.h
@@ -3,59 +3,61 @@
 
 #include <vector>
 
-template <typename T>
+template <class T>
 class SegmentTree {
 public:
-    template <typename T>
-    SegmentTree(int data_size) {
-        height = (int)(ceil(log2(data_size)));
-        size = 2 * (int)pow(2, height) - 1;
-        st = new int[size];
-    }
-
-    template <typename T>
-    ~SegmentTree() {
-        delete[] st;
-        st = NULL;
-    }
-
-    template <typename T>
-    void build_tree(std::vector<T> &data) {
-        build_tree_til(data, 0, data.size() - 1, 0);
-    }
-
-    template <typename T> 
-    void build_tree(T* data, int data_size) {
-        std::vector<T> data_vector (data, data + data_size);
-        build_tree_til(data_vector, 0, data_size - 1, 0);
-    }
-
-    template <typename T> 
-    T get_index(int index) {
-        return st[index];
-    }
+    SegmentTree(int data_size);
+    ~SegmentTree();
+    void build_tree(std::vector<T> &data);
+    void build_tree(T* data, int data_size);
+    int get_index(int index);
                                   
 private:
-    template <typename T> 
-    int build_tree_til(std::vector<T> &data, int start, int end, int index) {
-        if (start == end) {
-            st[index] = start;
-            return start;
-        }
-        int middle = (start + end) / 2;
-        int left_index = build_tree_til(data, start, middle, 2 * index + 1);
-        int right_index = build_tree_til(data, middle + 1, end, 2 * index + 2);
-        if (data[left_index] < data[right_index]) {
-            st[index] = left_index;
-        } else {
-            st[index] = right_index;
-        }
-        return st[index];
-    }
+    int build_tree_til(std::vector<T> &data, int start, int end, int index);
 
     int *st;
     int height;
     int size;
 };
+
+template <class T> SegmentTree<T>::SegmentTree(int data_size) {
+    height = (int)(ceil(log2(data_size)));
+    size = 2 * (int)pow(2, height) - 1;
+    st = new int[size];
+}
+
+template <class T> SegmentTree<T>::~SegmentTree() {
+    delete[] st;
+    st = NULL;
+}
+
+template <class T> SegmentTree<T>::void build_tree(std::vector<T> &data) {
+    build_tree_til(data, 0, data.size() - 1, 0);
+}
+
+template <class T> SegmentTree<T>::void build_tree(T* data, int data_size) {
+    std::vector<T> data_vector (data, data + data_size);
+    build_tree_til(data_vector, 0, data_size - 1, 0);
+}
+
+template <class T> SegmentTree<T>::int get_index(int index) {
+    return st[index];
+}
+
+template <class T> SegmentTree<T>::int build_tree_til(std::vector<T> &data, int start, int end, int index) {
+    if (start == end) {
+        st[index] = start;
+        return start;
+    }
+    int middle = (start + end) / 2;
+    int left_index = build_tree_til(data, start, middle, 2 * index + 1);
+    int right_index = build_tree_til(data, middle + 1, end, 2 * index + 2);
+    if (data[left_index] < data[right_index]) {
+        st[index] = left_index;
+    } else {
+        st[index] = right_index;
+    }
+    return st[index];
+}
 
 #endif
